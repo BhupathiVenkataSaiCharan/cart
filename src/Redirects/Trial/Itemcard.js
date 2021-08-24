61573c8ff1860f1317b7e042a8dc1c6e9b8fa98b
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-08-24 20:53:41 +0530

    my first commit reactwebsite

---
+++ src/Redirects/Trial/Itemcard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { useCart } from "react-use-cart";
+
+const Itemcard = (props) => {
+    
+    const { addItem } = useCart();
+
+    return(
+        <div className="row" >
+            <div className="col-sm-6">
+                <div className="card bg-opacity-50">
+                <div className="card-body text-center" >
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text"> Rs. {props.price}</p>
+                    <button
+                        style={{position:'relative',
+                                                    width:'80px',
+                                                    fontSize:'15px',
+                                                    padding:'10px',
+                                                    borderRadius:'10px',
+                                                    margin:'10px',
+                                                    textAlign:'center'}} 
+                                                    onClick={()=>addItem(props.item)} >+</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Itemcard;
